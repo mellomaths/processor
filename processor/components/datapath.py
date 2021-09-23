e996cd22746866223353d677dfeb1b9363bdb471
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2021-09-23 12:37:21 -0300

    Add Processor Datapath

---
+++ processor/components/datapath.py
@@ -0,0 +1,13 @@
+from processor.components.alu import ALU
+from processor.components.control_unit import ControlUnit
+from processor.components.register_file import RegisterFile
+from processor.components.program_counter import ProgramCounter 
+
+
+class Datapath:
+    
+    def __init__(self, bits: int) -> None:
+        self.register_file = RegisterFile(bits)
+        self.program_counter = ProgramCounter()
+        self.control_unit = ControlUnit()
+        self.ALU = ALU()
--- processor/processor.py
+++ processor/processor.py
@@ -1,4 +1,4 @@
-from processor.components.register_file import RegisterFile
+from processor.components.datapath import Datapath
 from processor.instructions.instruction_execution_chain import InstructionExecutionChain
 
 
@@ -6,7 +6,7 @@ class Processor:
 
     def __init__(self, bits: int) -> None:
         self.instruction_execution_chain = InstructionExecutionChain()
-        self.register_file = RegisterFile(bits)
+        self.datapath = Datapath(bits)
 
     def execute(self):
         self.instruction_execution_chain.execute()
